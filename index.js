8a8897b4e70f8d299b76c85c13dba596062e599f
EnriqueGmz <enriquegomezs@gmail.com>
2022-09-28 10:53:25 +0200

    Inicio de conexion

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import mysqlConnect from "./database/connectdb.js";
+import morgan from "morgan";
+import authRouter from "./routes/auth.route.js"
 
 const app = express();
 dotenv.config();
 
+app.use("/", authRouter)
+
+// Middlewares
+app.use(express());
+app.use(morgan("dev"));
+
+
+// Puerto conectado
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server on port: http://localhost:${PORT} 🚀🚀🚀`));
